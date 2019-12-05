a4d871c9022cad52a51b870cbaedc97c18f0a5f6
ShreyansGupta <50500026+ShreyansGupta@users.noreply.github.com>
2019-12-05 11:19:18 -0500

    Changes Updated

--- cnn.py
+++ cnn.py
@@ -1,99 +1,126 @@
 import numpy as np
 
+
 class CNN:
-	def __init__:
-		image_size = 28 
-		input_channels = 1
-		output_channels = 3
-		filter_size = 3
-		no_of_filters1 = 32
-		no_of_filters2 = 64
-		learning_rate = 0.1
+    def __init__(self):
+        self.image_size = 28
+        self.input_channels = 1
+        self.output_channels = 3
+        self.filter_size = 3
+        self.no_of_filters1 = 32
+        self.no_of_filters2 = 64
+        self.learning_rate = 0.1
+        self.parameters = {}
 
-	def initialize_parameters():
-		mean = 0
-		std = 0.5
+    def initialize_parameters(self):
+        mean = 0
+        std = 0.5
+		# Con-Layer1 Filter
+        self.parameters['conv_layer1_weights'] = np.random.normal(mean, std, (
+        self.filter_size, self.filter_size, self.input_channels, self.no_of_filters1))
+		# Layer1 Bias
+        self.parameters['conv_layer1_biases'] = np.zeros((1, self.no_of_filters1))
+		# Layer2 Filter
+        self.parameters['conv_layer2_weights'] = np.random.normal(mean, std, (
+        self.filter_size, self.filter_size, self.no_of_filters1, self.no_of_filters2))
+		# Layer2 Bias
+        self.parameters['conv_layer2_biases'] = np.zeros((1, self.no_of_filters2))
+		# Decon-Layer1 Filter
+        self.parameters['deconv_layer1_weights'] = np.random.normal(mean, std, (
+        self.filter_size, self.filter_size, self.no_of_filters2, self.no_of_filters1))
+		# Decon Layer Bias
+        self.parameters['deconv_layer1_biases'] = np.zeros((1, self.no_of_filters2))
+		# Decon Layer2 Filter
+        self.parameters['deconv_layer2_weights'] = np.random.normal(mean, std, (
+        self.filter_size, self.filter_size, self.no_of_filters1, self.output_channels))
+		# Decon Layer2 Bias
+        self.parameters['deconv_layer2_biases'] = np.zeros((1, self.no_of_filters1))
 
-		conv_layer1_weights = np.random.normal(mean, std,(filter_size, filter_size, input_channels, no_of_filters1))
-		conv_layer1_biases = np.zeros((1, no_of_filters1))
-		conv_layer2_weights = np.random.normal(mean, std,(filter_size, filter_size, no_of_filters1, no_of_filters2))
-		conv_layer2_biases = np.zeros((1, no_of_filters2))	
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-1 * x))
 
-		deconv_layer1_weights = np.random.normal(mean, std,(filter_size, filter_size, input_channels, no_of_filters1))
-		deconv_layer1_biases = np.zeros((1, no_of_filters1))
-		deconv_layer2_weights = np.random.normal(mean, std,(filter_size, filter_size, no_of_filters1, no_of_filters2))
-		deconv_layer2_biases = np.zeros((1, no_of_filters2))
+    def sigmoid_derv(self, x):
+        return self.sigmoid(x) * (1 - self.sigmoid(x))
 
+    def conv(self, X, w, b):
+        return np.sum(np.multiply(w, X)) + b
 
-	def sigmoid(x):
-		return 1/(1+np.exp(-1*x))
+    def conv_layer(self, image, filter, bias, toPad):
+        if toPad == True:
+            pad = (self.filter_size - 1) // 2
+            image = np.pad(image, ((0, 0), (pad, pad), (pad, pad), (0, 0)), 'constant', constant_values=0)
 
-	def sigmoid_derv(x):
-		return sigmoid(x)*(1-sigmoid(x))
+        m, row, col, channels = image.shape
+        nf = filter.shape[3]
+        new_image = np.zeros((m, row - self.filter_size + 1, col - self.filter_size + 1, nf))
 
+        for i in range(m):
+            img = image[i]
+            for r in range(row - self.filter_size + 1):
+                for c in range(col - self.filter_size + 1):
+                    x = img[r:r + self.filter_size, c:c + self.filter_size, :]
+                    for f in range(nf):
+                        new_image[i, r, c, f] = self.conv(x, filter[:, :, :, f], bias[:, f])
+        return new_image
 
-	def conv(X,w,b):
-		return np.sum(np.multiply(w,X))+b
-
-	def conv_layer(image, filter, bias):
-		m,row,col,channels = image.shape
-		nf = filter.shape[3]
-		new_image = np.zeros((m, row-filter_size+1, col-filter_size+1, nf))
-
-		for i in range(m):
-			img = image[i]
-			for r in range(row-filter_size+1):
-				for c in range(col-filter_size+1):
-					x = img[r:r+filter_size, c:c+filter_size, :]
-					for f in range(nf):
-						new_image[i,r,c,f] = conv(x,filter[:,:,:,f],bias[:,f])
-		return new_image
-
-
-	def maxPool(self,image):
-        count_images,row,col,channels = image.shape
-        new_row=row//2
-        new_col=col//2
-        new_image=np.zeros((count_images,new_row,new_col,channels))
+    def maxpool(self, image):
+        count_images, row, col, channels = image.shape
+        new_row = row // 2
+        new_col = col // 2
+        new_image = np.zeros((count_images, new_row, new_col, channels))
 
         for i in range(count_images):
-            img=image[i]
+            img = image[i]
             for r in range(new_row):
                 for c in range(new_col):
                     for d in range(channels):
-                         new_image[i,r,c,d]=np.max(img[2*r:2*r+2,2*c:2*c+2,d])
+                        new_image[i, r, c, d] = np.max(img[2 * r:2 * r + 2, 2 * c:2 * c + 2, d])
         return new_image
 
+    def deconv_layer(self, image, filter, bias):
+        pad = (self.filter_size - 1) // 2
+        image_pad = np.pad(image, ((0, 0), (pad, pad), (pad, pad), (0, 0)), 'constant', constant_values=0)
+        new_image = self.conv_layer(image_pad, filter, bias, False)
+        return new_image
 
-    def deconv_layer(image,filter,bias):
-    	m,row,col,channels = image.shape
-    	pad = filter_size -1 
-    	image_pad = np.zeros((m, row+2*pad, col+2*pad, channels))
-		image_pad[:,pad+1:row+pad+1,pad+1:col+pad+1,:] = image
-		new_image = conv_layer(image_pad, filter,bias)
-		return new_image
-
-
-    def unPooling(self,image):
-        count_image,row,col,channels=image.shape
-        new_image=np.zeros((count_image,2*row,2*col,channels))
+    def unPooling(self, image):
+        count_image, row, col, channels = image.shape
+        new_image = np.zeros((count_image, 2 * row, 2 * col, channels))
         for i in range(count_image):
-            img=image[i]
+            img = image[i]
             for r in range(row):
                 for c in range(col):
                     for d in range(channels):
-                        new_image[i,2*r:2*r+2,2*c:2*c+2,d]=img[r,c,d]                
-		return new_image
+                        new_image[i, 2 * r:2 * r + 2, 2 * c:2 * c + 2, d] = img[r, c, d]
+        return new_image
 
+    def forward_prop(self, image):
+        self.initialize_parameters()
+        print(image.shape)
+        conv_layer1 = self.conv_layer(image, self.parameters['conv_layer1_weights'],
+                                      self.parameters['conv_layer1_biases'], True)
+        print(conv_layer1.shape)
+        maxpool1 = self.maxpool(conv_layer1)
+        print(maxpool1.shape)
+        conv_layer2 = self.conv_layer(maxpool1, self.parameters['conv_layer2_weights'],
+                                      self.parameters['conv_layer2_biases'], True)
+        print(conv_layer2.shape)
+        maxpool2 = self.maxpool(conv_layer2)
+        print(maxpool2.shape)
+        unpool1 = self.unPooling(maxpool2)
+        print(unpool1.shape)
+        deconv_layer1 = self.deconv_layer(unpool1, self.parameters['deconv_layer1_weights'],
+                                          self.parameters['deconv_layer1_biases'])
+        print(deconv_layer1.shape)
+        unmaxpool2 = self.unPooling(deconv_layer1)
+        print(unmaxpool2.shape)
+        deconv_layer2 = self.deconv_layer(unmaxpool2, self.parameters['deconv_layer2_weights'],
+                                          self.parameters['deconv_layer2_biases'])
+        print(deconv_layer2.shape)
+        return deconv_layer2
 
-	def forward_prop(image):
-		initialize_parameters()
-		conv_layer1 = conv_layer(image, conv_layer1_weights, conv_layer1_biases)
-		maxpool1 = maxpool(conv_layer1)
-		conv_layer2 = conv_layer(maxpool1, conv_layer2_weights, conv_layer2_biases)
-		maxpool2 = maxpool(conv_layer2)
-		deconv_layer1 = deconv_layer(maxpool2, deconv_layer1_weights, deconv_layer1_biases)
-		unmaxpool1 = unmaxpool(deconv_layer1)
-		deconv_layer2 = deconv_layer(maxpool1, deconv_layer2_weights, deconv_layer2_biases)
-		unmaxpool2 = unmaxpool(deconv_layer2)
-
+# image = np.random.randn(1,32,32,1)
+# cnn = CNN()
+# cnn.initialize_parameters()
+# new_image = cnn.forward_prop(image)
+# print(new_image.shape)
