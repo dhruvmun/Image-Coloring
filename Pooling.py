7306b350364aadf26dd9006c67e55e3e0ee042d1
ShreyansGupta <50500026+ShreyansGupta@users.noreply.github.com>
2019-12-04 19:18:31 -0500

    Unpooling Method

--- Pooling.py
+++ Pooling.py
@@ -17,8 +17,20 @@ class Pooling:
                 for c in range(new_col):
                     for d in range(channels):
                          new_image[i,r,c,d]=findMax(img[r:r+2,c:c+2,d])
-
+        return new_image
 
 
     def findMax(self,image):
         return np.max(image)
+
+    def unPooling(self,image):
+        count_image,row,col,channels=image.shape
+        new_image=np.zeros([count_image,2*row,2*col,channels])
+        for i in range(count_image):
+            img=image[i]
+            for r in range(row):
+                for c in range(col):
+                    for d in range(channels):
+                        new_image[i,r:r+2,c:c+2,d]=img[r,c,d]
+                        
+        return new_image
